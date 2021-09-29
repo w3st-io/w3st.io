db8790d85c1ef02ef420eade0a8a247aff71bd13
aleem-ahmed <opall.io2020@gmail.com>
2021-09-29 10:56:15 -0400

    Improved

--- client/src/pages/services/social-media-management.vue
+++ client/src/pages/services/social-media-management.vue
@@ -1,47 +1,50 @@
 <template>
 	<BContainer class="my-5">
-		<BRow>
-			<BCol cols="12">
-				<!-- [WEB-APP-HOSTING] -->
-				<BCard
-					bg-variant="dark"
-					text-variant="primary"
-					class="mb-5 shadow table-holder"
-				>
-					<BRow>
-						<BCol cols="12">
-							<h3 class="mb-5 text-center text-primary">
-								{{ pageData.services[2].name }}
-							</h3>
-						</BCol>
+		<!-- [WEB-APP-HOSTING] -->
+		<BCard
+			bg-variant="dark"
+			text-variant="light"
+			class="mb-5 shadow table-holder"
+		>
+			<BRow>
+				<BCol cols="12">
+					<h3 class="mb-5 text-center text-primary">
+						{{ pageData.services[2].name }}
+					</h3>
+				</BCol>
 
-						<BCol cols="12" md="6">
-							<p class="h5 mb-4 p-5 text-light">
-								{{ pageData.services[2].descriptions[0] }}
-							</p>
-						</BCol>
+				<BCol cols="12" xl="6">
+					<p class="p-2 p-xl-5">
+						{{ pageData.services[2].descriptions[0] }}
+					</p>
+				</BCol>
 
-						<BCol cols="12" md="6">
-							<BListGroup class="mx-auto">
-								<BListGroupItem class="bg-dark text-primary">
-									<h5 class="m-0 text-center">
-										What You Get From Our SMM Service
-									</h5>
-								</BListGroupItem>
+				<BCol cols="12" xl="6">
+					<BListGroup class="mx-auto mb-3">
+						<BListGroupItem class="bg-dark text-primary">
+							<h5 class="m-0 text-center">
+								What You Get From Our SMM Service
+							</h5>
+						</BListGroupItem>
 
-								<BListGroupItem
-									v-for="(f, i) in pageData.services[2].features"
-									:key="i"
-									class="text-light bg-dark"
-								>
-									{{ f }}
-								</BListGroupItem>
-							</BListGroup>
-						</BCol>
-					</BRow>
-				</BCard>
-			</BCol>
-		</BRow>
+						<BListGroupItem
+							v-for="(f, i) in pageData.services[2].features"
+							:key="i"
+							class="text-light bg-dark"
+						>- {{ f }}</BListGroupItem>
+					</BListGroup>
+				</BCol>
+
+				<BCol cols="12">
+					<RouterLink to="/contact">
+						<BButton
+							variant="primary"
+							class="w-100"
+						>Contact</BButton>
+					</RouterLink>
+				</BCol>
+			</BRow>
+		</BCard>
 	</BContainer>
 </template>
 
--- client/src/pages/services/web-app-development.vue
+++ client/src/pages/services/web-app-development.vue
@@ -1,92 +1,107 @@
 <template>
 	<BContainer class="my-5">
 		<!-- [WEB-APP-DEVELOPMENT] -->
-		<BRow class="mx-1 shadow">
-			<BCol cols="12" class="bg-dark text-light">
-				<div class="py-3">
-					<h3 class="text-center text-primary">
-						{{ pageData.services[0].name }}
-					</h3>
-					<h6 class="m-0 small text-center">
-						{{ pageData.services[0].disclaimer }}
-					</h6>
-				</div>
-			</BCol>
+		<BCard
+			bg-variant="dark"
+			text-variant="light"
+			class="mb-5 shadow table-holder"
+		>
+			<BRow>
+				<BCol cols="12" class="bg-dark text-light">
+					<div class="py-3">
+						<h3 class="text-center text-primary">
+							{{ pageData.services[0].name }}
+						</h3>
+						<h6 class="m-0 small text-center">
+							{{ pageData.services[0].disclaimer }}
+						</h6>
+					</div>
+				</BCol>
 
-			<BCol cols="12" xl="4" class="bg-dark text-light">
-				<div class="p-2 p-xl-5">
-					<p>{{ pageData.services[0].descriptions[0] }}</p>
-				</div>
-			</BCol>
+				<BCol cols="12" xl="4" class="bg-dark text-light">
+					<div class="p-2 p-xl-5">
+						<p>{{ pageData.services[0].descriptions[0] }}</p>
+					</div>
+				</BCol>
 
-			<BCol cols="12" xl="8" class="bg-dark">
-				<div class="p-3 table-holder">
-					<table class="m-0 table table-striped table-dark">
-						<thead class="">
-							<tr class="text-center font-weight-bold text-primary">
-								<td></td>
-								<td>
-									{{ pageData.services[0].variants[0].name }}
-								</td>
-								<td>
-									{{ pageData.services[0].variants[1].name }}
-								</td>
-								<td>
-									{{ pageData.services[0].variants[2].name }}
-								</td>
-							</tr>
-						</thead>
+				<BCol cols="12" xl="8" class="bg-dark">
+					<div class="p-3 table-holder">
+						<table class="m-0 table table-striped table-dark">
+							<thead class="">
+								<tr class="text-center font-weight-bold text-primary">
+									<td></td>
+									<td>
+										{{ pageData.services[0].variants[0].name }}
+									</td>
+									<td>
+										{{ pageData.services[0].variants[1].name }}
+									</td>
+									<td>
+										{{ pageData.services[0].variants[2].name }}
+									</td>
+								</tr>
+							</thead>
 
-						<tbody class="text-center font-weight-bold text-primary">
-							<tr>
-								<td>Complete Informative Pages</td>
-								<td>✓</td>
-								<td>✓</td>
-								<td>✓</td>
-							</tr>
-							<tr>
-								<td>Blog</td>
-								<td>✓</td>
-								<td>✓</td>
-								<td>✓</td>
-							</tr>
-							<tr>
-								<td>Full SEO</td>
-								<td>✓</td>
-								<td>✓</td>
-								<td>✓</td>
-							</tr>
-							<tr>
-								<td>Online Store</td>
-								<td></td>
-								<td>✓</td>
-								<td>✓</td>
-							</tr>
-							<tr>
-								<td>Mobile App</td>
-								<td></td>
-								<td></td>
-								<td>✓</td>
-							</tr>
-							<tr>
-								<td class="border-top border-primary">
-									Price
-								</td>
-								<td class="border-top border-primary">
-									{{ pageData.services[0].variants[0].price }}
-								</td>
-								<td class="border-top border-primary">
-									{{ pageData.services[0].variants[1].price }}
-								</td>
-								<td class="border-top border-primary">
-									{{ pageData.services[0].variants[2].price }}
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</BCol>
-		</BRow>
+							<tbody class="text-center font-weight-bold text-primary">
+								<tr>
+									<td>Complete Informative Pages</td>
+									<td>✓</td>
+									<td>✓</td>
+									<td>✓</td>
+								</tr>
+								<tr>
+									<td>Blog</td>
+									<td>✓</td>
+									<td>✓</td>
+									<td>✓</td>
+								</tr>
+								<tr>
+									<td>Full SEO</td>
+									<td>✓</td>
+									<td>✓</td>
+									<td>✓</td>
+								</tr>
+								<tr>
+									<td>Online Store</td>
+									<td></td>
+									<td>✓</td>
+									<td>✓</td>
+								</tr>
+								<tr>
+									<td>Mobile App</td>
+									<td></td>
+									<td></td>
+									<td>✓</td>
+								</tr>
+								<tr>
+									<td class="border-top border-primary">
+										Price
+									</td>
+									<td class="border-top border-primary">
+										{{ pageData.services[0].variants[0].price }}
+									</td>
+									<td class="border-top border-primary">
+										{{ pageData.services[0].variants[1].price }}
+									</td>
+									<td class="border-top border-primary">
+										{{ pageData.services[0].variants[2].price }}
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</BCol>
+
+				<BCol cols="12">
+					<RouterLink to="/contact">
+						<BButton
+							variant="primary"
+							class="w-100"
+						>Contact</BButton>
+					</RouterLink>
+				</BCol>
+			</BRow>
+		</BCard>
 	</BContainer>
 </template>
 
