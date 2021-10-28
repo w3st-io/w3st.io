23a07cb3eda07913ad818ba73d35f04310bf683b
aleem-ahmed <opall.io2020@gmail.com>
2021-10-28 13:19:44 -0400

    Improved

--- client/src/defaults/client-projects.js
+++ client/src/defaults/client-projects.js
@@ -5,14 +5,24 @@ module.exports = [
 		link: 'https://www.jjinthemaking.com' 
 	},
 	{
-		image: 'https://images2.imgbox.com/dc/b1/X2zhrG2c_o.png',
-		title: '24-7 Fire Protection',
-		link: 'https://www.24-7fireprotection.com' 
+		image: 'https://images2.imgbox.com/63/8b/WPGVTmOl_o.png',
+		title: 'AISONI & CO',
+		link: 'https://aisoni.herokuapp.com' 
 	},
 	{
 		image: 'https://images2.imgbox.com/9d/9f/wCLwHAKF_o.png',
 		title: 'Balance Within',
 		link: 'https://www.balancewithpamela.com' 
+	},
+	{
+		image: 'https://images2.imgbox.com/36/06/hpMk1pQ1_o.png',
+		title: 'Dino\'s Bar & Grill',
+		link: 'https://dinosbar.herokuapp.com' 
+	},
+	{
+		image: 'https://images2.imgbox.com/dc/b1/X2zhrG2c_o.png',
+		title: '24-7 Fire Protection',
+		link: 'https://www.24-7fireprotection.com' 
 	},
 	{
 		image: 'https://images2.imgbox.com/f3/8f/iWDuBgBF_o.png',
