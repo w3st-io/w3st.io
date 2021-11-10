eab5578dc646860f7f49f6ffeee5faa5cc98a9d8
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-11-10 10:20:43 -0500

    s

--- client/src/pages/about.vue
+++ client/src/pages/about.vue
@@ -1,152 +1,112 @@
-// [EXPORT] //
-module.exports = {
-	logo: require('../assets/images/logo.svg'),
+<template>
+	<BContainer class="py-6">
+		<BCard
+			bg-variant="dark"
+			text-variant="light"
+			no-body
+			class="m-auto shadow bg-transparent"
+			border-variant="primary"
+		>
+			<BCardHeader class="border-bottom border-primary">
+				<h1 class="my-4 text-center text-primary text-uppercase text-spread-sm">
+					About <span v-html="companyInfo.nameHTML"></span>
+				</h1>
+			</BCardHeader>
 
-	name: 'W3ST.IO',
+			<BCardBody class="py-5">
+				<BRow>
+					<BCol cols="12" lg="6">
+						<div class="mx-lg-5 my-5">
+							<h3 class="mb-4 text-center text-uppercase text-primary">
+								Who We Are
+							</h3>
+							<p
+								class="mb-5 mx-auto h5 text-center font-weight-bold"
+								style="max-width: 600px;"
+							>{{ companyInfo.description }}</p>
 
-	nameHTML: '<span class="text-primary">W<span class="text-light">3</span>ST<span class="text-light">.</span>IO</span>',
+							<div class="text-center">
+								<RouterLink to="/contact">
+									<BButton
+										variant="outline-primary"
+										size="lg"
+										class="font-weight-bold"
+										pill
+									>Contact Us Today</BButton>
+								</RouterLink>
+							</div>
+						</div>
+					</BCol>
+					
+					<BCol cols="12" lg="6">
+						<lottie-player
+							src="https://assets5.lottiefiles.com/packages/lf20_3jmvq04g.json"
+							class="my-5 w-100 text-center mx-auto lottie"
+							background="transparent"
+							speed="1"
+							loop
+							autoplay
+						/>
+					</BCol>
+					
+					<BCol cols="12" lg="6">
+						<div class="my-5 text-center">
+							<img
+								src="https://images2.imgbox.com/f3/2f/7z8mFJYA_o.png"
+								class="d-none d-lg-block w-100 text-center mx-auto"
+								style="max-width: 300px;"
+							/>
+						</div>
+					</BCol>
 
-	caption: 'Nothing is impossible in the W3ST',
+					<BCol cols="12" lg="6">
+						<div class="mx-lg-5 my-5">
+							<h3 class="mb-4 text-center text-uppercase text-primary">
+								What We Believe
+							</h3>
+							<p
+								class="mb-5 mx-auto h5 text-center font-weight-bold"
+								style="max-width: 600px;"
+							>{{ companyInfo.missionStatement }}</p>
 
-	description: 'Established in 2021, W3ST.IO LLC is a web services company that provides its clients with an array of skillsets that help them grow their digital presence online. We understand how important it is for a business to truly connect with its community, so we make sure to meet all of our client’s business needs. We are constantly growing and want your business to grow with us as well..',
+							<div class="text-center">
+								<RouterLink to="/services">
+									<BButton
+										variant="outline-primary"
+										size="lg"
+										class="font-weight-bold"
+										pill
+									>Learn More</BButton>
+								</RouterLink>
+							</div>
+						</div>
+					</BCol>
+				</BRow>
+			</BCardBody>
+		</BCard>
+	</BContainer>
+</template>
 
-	missionStatement: 'W3ST stands for Web 3 Services & Technologies because we believe in the 3rd stage of the internet also called the Decentralized Internet. We want to guide all of our clients to this new internet and help them integrate the future into their businesses today. Our preferred blockchain is Ethereum.',
+<script>
+	import companyInfo from '../defaults/companyInfo'
 
-	address: '724 Cedar Lane, Teaneck NJ',
-
-	googleMapsLink: '',
-	
-	hoursOfOperation: [
-		{
-			days: 'Monday - Friday',
-			hours: '8:00 AM - 4:30 PM'
+	export default {
+		data() {
+			return {
+				companyInfo: companyInfo
+			}
 		},
-		{
-			days: 'Saturday',
-			hours: '9:00 AM - 1:00 PM'
-		},
-	],
+	}
+</script>
 
-	contact: {
-		cell: {
-			number: '(201) 362-6859',
-			link: 'tel:201-362-6859'
-		},
-	
-		fax: {
-			number: '(123) 456-7890',
-			link: 'tel:123-345-7890',
-		},
-	},
+<style lang="scss" scoped>
+	.lottie {
+		width: 100%;
+		max-height: 300px;
 
-	socialMedia: {
-		facebookLink: '',
-		instagramLink: '',
-		twitterLink: '',
-		youtubeLink: '',
-	},
-
-	services: [
-		{
-			id: 'web-app-development',
-
-			name: 'Web App Development (WAD)',
-
-			lottiefilesLink: 'https://assets6.lottiefiles.com/packages/lf20_8zzfnwv8.json',
-
-			variants: [
-				{
-					id: 'standard-web-app-development',
-					name: 'Standard Web App Development',
-					price: '$1,999.99',
-				},
-				{
-					id: 'advanced-web-app-development',
-					name: 'Advanced Web App Development',
-					price: '$2,999.99',
-				},
-				{
-					id: 'pro-web-app-development',
-					name: 'Pro Web App Development',
-					price: '$5,999.99',
-				},
-			],
-
-			descriptions: [
-				'A highly customizable website that is designed to help our clients reach out to their customers and share their products and services. We also provide the option to add an online store that can makes sales for your business anywhere! All Web Apps come with Fully Designed UI, a Blog System, and Search Engine Optimization (SEO).',
-			],
-
-			features: [],
-
-			disclaimer: 'DISCLAIMER: Web app code only. hosting not included.',
-		},
-		{
-			id: 'web-app-hosting',
-
-			name: 'Web App Hosting (WAH)',
-
-			lottiefilesLink: 'https://assets2.lottiefiles.com/packages/lf20_3jezq8s4.json',
-
-			variants: [
-				{
-					id: 'standard-web-app-hosting',
-					name: 'Standard Web App Hosting',
-					price: '$59.99 /mo',
-				},
-				{
-					id: 'advanced-web-app-hosting',
-					name: 'Advanced Web App Hosting',
-					price: '$89.99 /mo',
-				},
-				{
-					id: 'pro-web-app-hosting',
-					name: 'Pro Web App Hosting',
-					price: '$199.99 /mo',
-				},
-			],
-
-			descriptions: [
-				'All Web App needs to be hosted and we are here to help provide that for you. Our hosting service includes updating, maintaining, and improving the Web Apps we host. We make sure to work closely with our clients so that changes are up to their standards.',
-			],
-
-			features: [],
-
-			disclaimer: '',
-		},
-		{
-			id: 'social-media-management',
-			
-			name: 'Social Media Management (SMM)',
-
-			lottiefilesLink: 'https://assets10.lottiefiles.com/packages/lf20_q4m6E9.json',
-
-			variants: [
-				{
-					id: 'stanard-social-media-management',
-					name: 'Standard Social Media Management',
-					price: '$59.99 /mo',
-				},
-				{
-					id: 'pro-social-media-management',
-					name: 'Pro Social Media Management',
-					price: '$89.99 /mo',
-				},
-			],
-
-			descriptions: [
-				'A presence on social media is essential for any business to successfully engage with an audience who is constantly browsing, exploring, and contributing to the web. By collecting feedback from these platforms, a business can make better-informed decisions to navigate an ever-changing world. We provide the service of maintaining and collecting feedback from these platforms so you dont have too. Here is what we offer...',
-			],
-
-			features: [
-				'Upload new content every day (2 photos/videos a day)',
-				'Post updates and questionnaires on story',
-				'Interactions with customers by responding and answering their questions',
-				'Grabs feedback from the audience',
-				'Identify trends and create engaged content',
-				'Build an interactive digital community online',
-			],
-			
-			disclaimer: '',
+		@media (max-width: 576px) {
+			width: 200px;
+			height: 200px;
 		}
-	],
-}
+	}
+</style>
