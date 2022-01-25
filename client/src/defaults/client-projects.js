b3d594797a4dcaa486903d9fef520191d1f06a36
aleem-ahmed <w3st.io2021@gmail.com>
2022-01-25 18:29:15 -0500

    Improved

--- client/src/defaults/client-projects.js
+++ client/src/defaults/client-projects.js
@@ -1,8 +1,13 @@
 module.exports = [
 	{
-		image: 'https://images2.imgbox.com/f8/3f/4VIlG5Um_o.png',
-		title: 'JJ Shop',
-		link: 'https://www.jjinthemaking.com' 
+		image: 'https://images2.imgbox.com/94/80/qFy3R4tw_o.png',
+		title: 'Power Market',
+		link: 'https://www.pwrmarket.com' 
+	},
+	{
+		image: 'https://source.unsplash.com/800x400/?design',
+		title: 'Gordon Palmer',
+		link: 'https://www.gordonpalmer.com' 
 	},
 	{
 		image: 'https://images2.imgbox.com/56/52/v5IBaDhV_o.png',
@@ -10,19 +15,24 @@ module.exports = [
 		link: 'https://www.tethysapparel.com' 
 	},
 	{
-		image: 'https://images2.imgbox.com/63/8b/WPGVTmOl_o.png',
-		title: 'AISONI & CO',
-		link: 'https://aisoni.herokuapp.com' 
-	},
-	{
 		image: 'https://images2.imgbox.com/9d/9f/wCLwHAKF_o.png',
 		title: 'Balance Within',
 		link: 'https://www.balancewithpamela.com' 
 	},
 	{
+		image: 'https://images2.imgbox.com/63/8b/WPGVTmOl_o.png',
+		title: 'AISONI & CO',
+		link: 'https://aisoni.herokuapp.com' 
+	},
+	{
 		image: 'https://images2.imgbox.com/36/06/hpMk1pQ1_o.png',
 		title: 'Dino\'s Bar & Grill',
 		link: 'https://dinosbar.herokuapp.com' 
+	},
+	{
+		image: 'https://images2.imgbox.com/f8/3f/4VIlG5Um_o.png',
+		title: 'JJ Shop',
+		link: 'https://www.jjinthemaking.com' 
 	},
 	{
 		image: 'https://images2.imgbox.com/dc/b1/X2zhrG2c_o.png',
@@ -33,5 +43,10 @@ module.exports = [
 		image: 'https://images2.imgbox.com/f3/8f/iWDuBgBF_o.png',
 		title: 'The Art of Wings',
 		link: 'https://www.theartofwings.com' 
+	},
+	{
+		image: 'https://images2.imgbox.com/d2/1c/LLlHfKcC_o.png',
+		title: 'RpTide',
+		link: 'https://www.rptide.com' 
 	},
 ]
