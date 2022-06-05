70f01c7bdc3e46187ae5a0b283b9ab0641fd5901
aleem ahmed <w3st.io2021@gmail.com>
2022-06-05 14:33:03 -0400

    s

--- client/src/components/nav/Footer.vue
+++ client/src/components/nav/Footer.vue
@@ -2,7 +2,7 @@
 	<div class="w-100 py-6 text-light border-top border-secondary bg-svg-dark">
 		<BContainer>
 			<BRow>
-				<BCol cols="12" sm="4" class="text-center text-sm-left" data-aos="fade-up">
+				<BCol cols="12" sm="4" class="text-center text-sm-left" data-aos="fade-up" data-aos-once="true">
 					<h4 class="text-light">Location</h4>
 					<a :href="companyInfo.googleMapsLink" class="mb-3">
 						<h5 class="mb-4">{{ companyInfo.address }}</h5>
@@ -20,7 +20,7 @@
 					</ul>
 				</BCol>
 
-				<BCol cols="12" sm="4" class="text-center" data-aos="fade-up">
+				<BCol cols="12" sm="4" class="text-center" data-aos="fade-up" data-aos-once="true">
 					<h4>Company</h4>
 					<ul class="list-unstyled text-small">
 						<li>
@@ -56,7 +56,7 @@
 					</ul>
 				</BCol>
 
-				<BCol cols="12" sm="4" class="text-center" data-aos="fade-up">
+				<BCol cols="12" sm="4" class="text-center" data-aos="fade-up" data-aos-once="true">
 					<h4>Legal</h4>
 					<ul class="list-unstyled text-small">
 						<li>
