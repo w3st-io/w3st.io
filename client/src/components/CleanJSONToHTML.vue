8f48c7b9a9c48aea14afde81e6d75e2b1e7284b8
aleem ahmed <w3st.io2021@gmail.com>
2022-06-06 11:28:03 -0400

    Improved

--- client/src/components/CleanJSONToHTML.vue
+++ client/src/components/CleanJSONToHTML.vue
@@ -17,7 +17,7 @@
 					class="mx-auto"
 					style="max-width: 700px;"
 				/>
-				<p v-html="b.data.caption"></p>
+				<p v-html="b.data.caption" />
 			</div>
 
 			<!-- Headers -->
@@ -25,53 +25,64 @@
 				v-if="b.type == 'header' && b.data.level == 1"
 				v-html="b.data.text"
 				class="font-weight-bold text-primary"
-			></h1>
+			/>
 
 			<h2
 				v-if="b.type == 'header' && b.data.level == 2"
 				v-html="b.data.text"
 				class="font-weight-bold text-primary"
-			></h2>
+			/>
 
 			<h3
 				v-if="b.type == 'header' && b.data.level == 3"
 				v-html="b.data.text"
 				class="font-weight-bold text-primary"
-			></h3>
+			/>
 
 			<h4
 				v-if="b.type == 'header' && b.data.level == 4"
 				v-html="b.data.text"
 				class="font-weight-bold text-primary"
-			></h4>
+			/>
 
 			<h5
 				v-if="b.type == 'header' && b.data.level == 5"
 				v-html="b.data.text"
 				class="font-weight-bold text-primary"
-			></h5>
+			/>
 
 			<h6
 				v-if="b.type == 'header' && b.data.level == 6"
 				v-html="b.data.text"
 				class="font-weight-bold text-primary"
-			></h6>
+			/>
 
 			<!-- Image -->
-			<viewer
-				v-if="b.type == 'image'"
-				:options="{ title: false, transition: false, }"
-				class="my-4 text-center"
-			>
-				<img
-					:src="b.data.url"
-					v-lazy="b.data.url"
-					alt=""
-					class="w-100 mx-auto mb-2 shadow-sm"
-					style="max-width: 800px;"
+				<viewer
+					v-if="b.type == 'image'"
+					:options="{ title: false, transition: false, }"
 				>
-				<p v-html="b.data.caption" class="font-weight-bold"></p>
-			</viewer>
+					<div
+						class="
+							mx-auto
+							mt-4
+							mb-3
+							rounded
+							img-container
+							border
+							border-primary
+						"
+						style="max-width: 600px; border-width: 3px !important;"
+					>
+						<img
+							:src="b.data.url"
+							v-lazy="b.data.url"
+							alt=""
+							class="w-100"
+						>
+					</div>
+					<p v-html="b.data.caption" class="text-center font-weight-bold" />
+				</viewer>
 
 			<!-- List Unordered -->
 			<ul v-if="b.type == 'list' && b.data.style == 'unordered'" class="my-3">
@@ -80,7 +91,7 @@
 					:key="ii"
 					v-html="li"
 					class="mb-2"
-				></li>
+				/>
 			</ul>
 
 			<!-- List Ordered -->
@@ -90,24 +101,24 @@
 					:key="ii"
 					v-html="li"
 					class="mb-2"
-				></li>
+				/>
 			</ol>
 
 			<!-- Paragraph -->
-			<p v-if="b.type == 'paragraph'" v-html="b.data.text" class="mb-2"></p>
+			<p v-if="b.type == 'paragraph'" v-html="b.data.text" class="mb-2" />
 
 			<!-- Quote -->
 			<figure v-if="b.type == 'quote'" class="mx-auto" style="max-width: 800px;">
 				<blockquote>
-					<i v-html="b.data.text"></i>
+					<i v-html="b.data.text" />
 				</blockquote>
-				<figcaption v-html="b.data.caption"></figcaption>
+				<figcaption v-html="b.data.caption" />
 			</figure>
 
 			<!-- Table -->
 			<table v-if="b.type == 'table'" class="table table-striped table-dark">
 				<tr v-for="(row, index) in b.data.content" :key="index">
-					<td v-for="(col, index) in row" :key="index">{{ col }}</td>
+					<td v-for="(col, index) in row" :key="index" v-html="col" />
 				</tr>
 			</table>
 		</div>
@@ -134,5 +145,11 @@
 <style lang="scss" scoped>
 	h1,h2,h3,h4,h5,h6 {
 		margin: 25px 0 !important;
+		text-transform: uppercase;
+	}
+
+	.img-container {
+		max-height: 600px;
+		overflow: hidden;
 	}
 </style>
