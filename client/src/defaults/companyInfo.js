29d70b3f333bcad36436007dbcb4ccd85b40dcf0
aleem ahmed <w3st.io2021@gmail.com>
2022-06-07 11:40:33 -0400

    Backlit everything

--- client/src/defaults/companyInfo.js
+++ client/src/defaults/companyInfo.js
@@ -4,7 +4,7 @@ module.exports = {
 
 	name: 'W3ST.IO',
 
-	nameHTML: '<span class="text-dark">W<span class="text-light">3</span>ST<span class="text-light">.</span>IO</span>',
+	nameHTML: '<span class="text-dark">W<span class="text-light text-backlit">3</span>ST<span class="text-light text-backlit">.</span>IO</span>',
 
 	caption: '',
 
