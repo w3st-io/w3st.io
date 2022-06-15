2ede83b987416b268fa51839bb04927549fc2695
aleem-ahmed <w3st.io2021@gmail.com>
2022-06-15 09:28:18 -0400

    Improved

--- s-routes/fun/index.ts
+++ s-routes/fun/index.ts
@@ -33,168 +33,124 @@ const html = `
 	>
 </head>
 
+<!-- Bootstrap Override -->
 <style>
-	* {
-		letter-spacing: 2px;
-		font-family: 'Ubuntu Condensed', sans-serif;
-		font-family: 'Orbitron', sans-serif;
-	}
+.bg-dark { background-color: black !important; }
 
-	.bg-dark { background-color: black !important; }
+.btn-outline-danger {
+	border-width: 3px;
+}
 
-	.btn-outline-danger {
-		border-width: 3px;
-	}
-
-	.space-text {
-		color: rgba(255, 255, 255, 0);
-		-webkit-text-stroke-width: 3px;
-		-webkit-text-stroke-color: rgb(255, 255, 255);
-	}
-
-	.space-text-h {
-		color: rgba(0, 0, 0, 0);
-		-webkit-text-stroke-width: 2px;
-		-webkit-text-stroke-color: rgb(0, 0, 0);
-	}
-
-	.space-text-h:hover {
-		background-color: #000000 !important;
-		color: rgb(255, 255, 255);
-	}
-
-	.thick-border {
-		border-width: 3px !important;
-	}
-	
-	.first-section {
-		border-width: 0px !important;
-	}
-
-	@media screen and (min-width: 992px) {
-		.logo { transform: translate(0px, 30px); }
-
-		.first-section {
-			border-width: 3px !important;
-		}
-	}
-
-	.logo:hover {
-		-webkit-text-stroke-color: rgb(0, 0, 0);
-	}
-
-	.mission-description {
-		overflow-y: scroll;
-		height: 150px;
-	}
-
-	.flashing {
-		animation-duration: 400ms;
-		animation-name: blink;
-		animation-iteration-count: infinite;
-		animation-direction: alternate;
-	 }
-
-	@keyframes blink {
-		from {
-			color: black;
-		}
-		to {
-			color: white;
-		}
-	}
-
-	.glow {
-		box-shadow: 0 0 20px 5px #f58383;
-	}
-
-	.hyper-jump {
-		animation: glow 1s infinite alternate;
-	}
-
-	@keyframes glow {
-		from {
-			box-shadow: 0 0 20px -5px #f58383;
-		}
-		to {
-			box-shadow: 0 0 20px 5px #f58383;
-		}
-	}
-
-	.deploy-button {
-		animation: deploy-button 1s infinite alternate;
-	}
-
-	@keyframes deploy-button {
-		from {
-			box-shadow: 0 0 10px 10px #aef4af;
-		}
-		to {
-			box-shadow: 0 0 10px -10px #aef4af;
-		}
-	}
-
-	.text-glow {
-		animation: text-shadow-glow 1s infinite alternate;
-	}
-
-	@keyframes text-shadow-glow {
-		from {
-			display: none;
-		}
-		to {
-			display: block;
-		}
-	}
-
-	.rocket-img {
-		/* Start the shake animation and make the animation last for 0.5 seconds */
-		animation: shake 1s infinite alternate;
-	}
-
-	@keyframes shake {
-		from { transform: translate(0, 10px) rotate(0deg); }
-		to { transform: translate(0, 0) rotate(0deg); }
-	}
+.container {
+	max-width: 1300px;
+}
 </style>
 
 <style>
-	.myProgress {
-		width: 100%;
-		background-color: white;
-		border: solid 3px;
+* {
+	letter-spacing: 2px;
+	font-family: 'Ubuntu Condensed', sans-serif;
+	font-family: 'Orbitron', sans-serif;
+}
+
+.space-text {
+	color: rgba(255, 255, 255, 0);
+	-webkit-text-stroke-width: 3px;
+	-webkit-text-stroke-color: rgb(255, 255, 255);
+}
+
+.space-text-h {
+	color: rgba(0, 0, 0, 0);
+	-webkit-text-stroke-width: 2px;
+	-webkit-text-stroke-color: rgb(0, 0, 0);
+}
+
+.space-text-h:hover {
+	background-color: #000000 !important;
+	color: rgb(255, 255, 255);
+}
+
+.thick-border {
+	border-width: 3px !important;
+}
+
+.status-section {
+	border-width: 0px !important;
+}
+
+@media screen and (min-width: 992px) {
+	.logo { transform: translate(0px, 30px); }
+
+	.status-section {
+		border-width: 3px !important;
 	}
-	
-	.myBar {
-		width: 30%;
-		height: 20px;
-	}
+}
+
+.logo:hover {
+	-webkit-text-stroke-color: rgb(0, 0, 0);
+}
+
+.mission-description {
+	overflow-y: scroll;
+	height: 150px;
+}
+
+/* Green Loading Bars */
+.myProgress {
+	width: 100%;
+	background-color: white;
+	border: solid 3px;
+}
+
+.myBar {
+	width: 30%;
+	height: 20px;
+}
 </style>
 
 <body>
 	<!-- NavBar -->
-	<div class="flashing">sdfsdf</div>
-	<div class="border-bottom border-dark thick-border">
+	<div class="border-bottom border-danger thick-border">
 		<div class="container">
 			<div class="row">
 				<div class="col-12 col-lg-4 mb-lg-5">
 					<div class="m-auto logo text-center">
 						<mark
-						class="m-0 py-0 bg-danger space-text glow"
-						style="font-size: 5em;"
+							class="m-0 py-0 bg-danger space-text glow"
+							style="font-size: 5em;"
 						>T.T.M.</mark>
 					</div>
 				</div>
 
-				<div class="col-12 col-lg-8 mt-3 py-3 border-top border-danger thick-border text-center">
-					<a href="https://www.w3st.io" class="text-dark text-decoration-none">
-						<span class="h3 mx-4 mb-3 space-text-h" style="font-size: 2.2em;">HOME</span>
+				<div class="col-12 col-lg-8 mt-3 py-3 border-top border-danger thick-border text-center text-uppercase">
+					<a
+						href="https://www.w3st.io"
+						class="text-dark text-decoration-none"
+					>
+						<span
+							class="h3 mx-4 mb-3 space-text-h"
+							style="font-size: 2.2em;"
+						>Home</span>
 					</a>
-					<a href="https://ethereum.org/en/developers/docs/" target="_blank" class="text-dark text-decoration-none">
-						<span class="h3 mx-4 mb-3 space-text-h" style="font-size: 2.2em;">DEPLOY</span>
+					<a
+						href="https://ethereum.org/en/developers/docs/"
+						target="_blank"
+						class="text-dark text-decoration-none"
+					>
+						<span
+							class="h3 mx-4 mb-3 space-text-h"
+							style="font-size: 2.2em;"
+						>Deploy</span>
 					</a>
-					<a href="https://cryptowat.ch" target="_blank" class="text-dark text-decoration-none">
-						<span class="h3 mx-4 mb-3 space-text-h" style="font-size: 2.2em;">ANALYZE</span>
+					<a
+						href="https://cryptowat.ch"
+						target="_blank"
+						class="text-dark text-decoration-none"
+					>
+						<span
+							class="h3 mx-4 mb-3 space-text-h"
+							style="font-size: 2.2em;"
+						>Analyze</span>
 					</a>
 				</div>
 			</div>
@@ -204,59 +160,76 @@ const html = `
 	<!-- Body -->
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-4 pt-5 border-right border-dark thick-border first-section">
-				<h2 class="text-danger text-uppercase">
+			<!-- Title -->
+			<div class="col-12 border-bottom border-dark thick-border">
+				<h1
+					class="my-4 px-2 text-light text-uppercase border bg-dark border-dark rounded thick-border"
+					>
+					<span>Mission:</span>
+					<span id="mission-name"></span>
+					<span id="mission-name-underscore" class="flashing">_</span>
+				</h1>		
+			</div>
+
+			<!-- Status Section -->
+			<div class="col-lg-4 border-right border-dark status-section thick-border text-uppercase">
+				<h2 class="mt-3 text-danger text-uppercase">
 					<span>Status</span>
 				</h2>
 
 				<h4 class="text-dark">
-					GAS FEE: <span id="myBarValue" class="text-success">78</span>
+					Gas Fee: <span id="myBarValue" class="text-success">78</span>
 				</h4>
 				<div id="myProgress" class="mb-3 myProgress">
-					<div id="myBar" class="myBar transition bg-success" style="transition: 2s;"></div>
+					<div
+						id="myBar"
+						class="myBar transition bg-success"
+						style="transition: 2s;"
+					></div>
 				</div>
 
 				<h4 class="text-dark">
-					CONFIRMATIONS: <span id="myBar1Value" class="text-success">34</span>
+					Confirmations: <span id="myBar1Value" class="text-success">34</span>
 				</h4>
 				<div id="myProgress" class="mb-3 myProgress">
-					<div id="myBar1" class="myBar transition bg-success" style="transition: 2s;"></div>
+					<div
+						id="myBar1"
+						class="myBar transition bg-success"
+						style="transition: 2s;"
+					></div>
 				</div>
 
 				<h4 class="text-dark">
 					TPS: <span id="myBar2Value" class="text-success">32</span>
 				</h4>
 				<div id="myProgress" class="mb-3 myProgress">
-					<div id="myBar2" class="myBar transition bg-success" style="transition: 2s;"></div>
+					<div
+						id="myBar2"
+						class="myBar transition bg-success"
+						style="transition: 2s;"
+					></div>
 				</div>
 
-				<h4 class="text-dark">SHIP STATUS:</h4>
-				<p class="text-success text-glow">&#9679; OPERATIONAL</p>
+				<h4 class="text-dark">Ship Status:</h4>
+				<p class="text-success">&#9679; Operational</p>
 
 				<img
 					src="https://images2.imgbox.com/f1/08/hvjgYjV7_o.jpg"
 					class="d-none d-lg-block mx-auto rocket-img w-100"
-					style="max-width: 200px;"
+					style="max-width: 230px;"
 				>
 			</div>
+			
+			<!-- Description Section -->
+			<div class="col-lg-8 border-dark thick-border">
+				<h2
+					class="my-3 px-2 text-dark border border-dark rounded thick-border text-uppercase"
+				>Destination: Moon</h2>
 
-			<div class="col-lg-8 pt-5 border-dark thick-border">
-				<div class="">
-					<h1
-						class="mb-5 px-2 text-dark text-uppercase border border-dark rounded"
-						style="border-width: 3px !important;"
-					>
-						<span>Mission:</span>
-						<span id="mission-name"></span>
-						<span id="mission-name-underscore">_</span>
-					</h1>
-
-					<p class="mb-5 p-2 bg-dark mission-description rounded">
-						<span id="mission-description" class="text-light"></span>
-						<span id="mission-name-underscore2">_</span>
-					</p>
-				</div>
-
+				<p class="mt-5 mb-5 p-2 bg-dark mission-description rounded">
+					<span id="mission-description" class="text-light"></span>
+					<span class="flashing">_</span>
+				</p>
 				<div class="row">
 					<div class="col-6">
 						<a href="https://ethereum.org/en/wallets/">
@@ -280,21 +253,23 @@ const html = `
 						<hr class="border-dark thick-border" style="border-style: dashed !important;">
 					</div>
 
-					<div class="col-12">
-						<h2
-							class="mb-5 px-2 text-dark border border-dark rounded"
-							style="border-width: 3px !important;"
-						>DESTINATION</h2>
-					</div>
+					<div class="col-lg-5 mb-5 text-uppercase">
+						<h4 class="text-dark">Launch Point:</h4>
+						<p class="p-2 bg-dark text-light rounded">
+							Earth (-115427)
+						</p>
 
-					<div class="col-lg-5 mb-5">
-						<h4 class="text-dark">PLANET NAME:</h4>
-						<p class="p-2 bg-dark text-light rounded">HARPOONJS.ETH</p>
+						<h4 class="text-dark">Time-Distance:</h4>
+						<p class="p-2 bg-dark text-light rounded">
+							34.657 LY
+						</p>
 
-						<h4 class="text-dark">DISTANCE:</h4>
-						<p class="p-2 bg-dark text-light rounded">34.657 LY</p>
+						<h4 class="text-dark">Astronaut:</h4>
+						<p class="p-2 bg-dark text-light rounded">
+							HARPOONJS.ETH
+						</p>
 
-						<h4 class="text-dark">TARGET:</h4>
+						<h4 class="text-dark">Target:</h4>
 						<p class="p-2 bg-dark text-light rounded">Ω (3,3)</p>
 					</div>
 
@@ -310,7 +285,6 @@ const html = `
 						<a href="https://github.com/w3st-io/cardinal" target="_blank">
 							<button
 								class="mb-5 btn btn-lg btn-danger w-100 hyper-jump"
-								style="border-radius: 30px;"
 							>HYPER-JUMP</button>
 						</a>
 					</div>
@@ -347,113 +321,82 @@ const html = `
 </html>
 
 <script>
-	const missionName = 'To the  moon'
-	
+	const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+</script>
+
+<!-- Mission Statement -->
+<script>	
+	const missionName = 'To the moon'
 	
 	const missionNameElement = document.getElementById('mission-name')
 
-	const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-
-	// Mission Name
-	const loop = async () => {
+	const traverseMissionName = async () => {
 		for (let i = 0; i < missionName.length; i++) {
-			const l = missionName[i]
-		
-			missionNameElement.innerHTML += l
+			missionNameElement.innerHTML += missionName[i]
 
 			await wait(100)
 		}
 	}
 
-	loop()
+	traverseMissionName()
+</script>
 
-	const underscore = document.getElementById('mission-name-underscore')
-
-	let displayed = true
-
-	function flash() {
-		if (displayed) {
-			underscore.style.color = 'black'
-		}
-		else {
-			underscore.style.color = 'white'
-		}
-		displayed = !displayed;
-	}
-	window.setInterval(flash, 500)
-	
-	// Mission Description
-	const missionDescriptionElement = document.getElementById('mission-description')
-
+<!-- Mission Description -->
+<script>
 	const missionDescription = 'W3ST stands for Web 3 Services & Technologies because we believe in the 3rd stage of the internet also called the Decentralized Internet. We want to guide all of our clients to this new internet and help them integrate the future into their businesses today. Our preferred blockchain is Ethereum.'
 	
-	const loop2 = async () => {
+	const missionDescriptionElement = document.getElementById('mission-description')
+	
+	const traverseMissionDescription = async () => {
 		for (let i = 0; i < missionDescription.length; i++) {
-			const l = missionDescription[i]
-		
-			missionDescriptionElement.innerHTML += l
+			missionDescriptionElement.innerHTML += missionDescription[i]
 
 			await wait(10)
 		}
 	}
 
-	loop2()
-
-	const underscore2 = document.getElementById('mission-name-underscore2')
-
-	let displayed2 = true
-
-	function flash2() {
-		if (displayed2) {
-			underscore2.style.color = 'black'
-		}
-		else {
-			underscore2.style.color = 'white'
-		}
-		displayed2 = !displayed2;
-	}
-	window.setInterval(flash2, 500)
+	traverseMissionDescription()
 </script>
 
 <!-- Gas Fee -->
 <script>
-	var width = 10;
-	var i = 0;
-	var elem = document.getElementById("myBar");
+	var width = 10
+	var i = 0
+	var elem = document.getElementById("myBar")
 
 	if (i == 0) {
-		i = 1;
+		i = 1
 
 		function frame() {
 			if (width >= 100) {
 				clearInterval(id)
-				i = 0;
+				i = 0
 			}
 			else {
-				width = Math.floor(Math.random() * 100);
+				width = Math.floor(Math.random() * 100)
 				
 				elem.style.width = width + "%"
 				document.getElementById("myBarValue").innerText = width
 			}
 		}
 
-		var id = setInterval(frame, 1000);
+		var id = setInterval(frame, 1000)
 	}
 </script>
 
 <!-- Blocks -->
 <script>
-	var width1 = 10;
-	var i1 = 0;
-	var elem1 = document.getElementById("myBar1");
+	var width1 = 10
+	var ii = 0
+	var elem1 = document.getElementById("myBar1")
 
-	if (i1 == 0) {
-		i1 = 1
+	if (ii == 0) {
+		ii = 1
 
 		function frame1() {
 			if (width1 >= 100) {
 				clearInterval(id1)
-				i1 = 0;
+				ii = 0
 			}
 			else {
 				width1 = Math.floor(Math.random() * 100);
@@ -463,23 +406,23 @@ const html = `
 			}
 		}
 
-		var id1 = setInterval(frame1, 1000);
+		var id1 = setInterval(frame1, 1000)
 	}
 </script>
 
 <!-- TPS -->
 <script>
-	var width2 = 10;
-	var i2 = 0;
-	var elem2 = document.getElementById("myBar2");
+	var width2 = 10
+	var iii = 0
+	var elem2 = document.getElementById("myBar2")
 
-	if (i2 == 0) {
-		i2 = 1;
+	if (iii == 0) {
+		iii = 1
 
 		function frame2() {
 			if (width2 >= 100) {
 				clearInterval(id2)
-				i2 = 0;
+				iii = 0
 			}
 			else {
 				width2 = Math.floor(Math.random() * 100)
@@ -489,9 +432,80 @@ const html = `
 			}
 		}
 
-		var id2 = setInterval(frame2, 1000);
+		var id2 = setInterval(frame2, 1000)
 	}
 </script>
+
+<style>
+.flashing {
+	animation-duration: 2s;
+	animation-name: blink;
+	animation-iteration-count: infinite;
+	animation-direction: alternate;
+}
+
+@keyframes blink {
+	0% {
+		color: black;
+	}
+	20% {
+		color: white;
+	}
+	40% {
+		color: black;
+	}
+	60% {
+		color: white;
+	}
+	80% {
+		color: black;
+	}
+	100% {
+		color: white;
+	}
+}
+
+.glow {
+	box-shadow: 0 0 20px 5px #f58383;
+}
+
+.hyper-jump {
+	animation: glow-red 1s infinite alternate;
+	border-radius: 30px;
+}
+
+@keyframes glow-red {
+	from {
+		box-shadow: 0 0 20px -5px #f58383;
+	}
+	to {
+		box-shadow: 0 0 20px 5px #f58383;
+	}
+}
+
+.deploy-button {
+	animation: deploy-button 1s infinite alternate;
+}
+
+@keyframes deploy-button {
+	from {
+		box-shadow: 0 0 10px 10px #aef4af;
+	}
+	to {
+		box-shadow: 0 0 10px -10px #aef4af;
+	}
+}
+
+.rocket-img {
+	/* Start the shake animation and make the animation last for 0.5 seconds */
+	animation: shake 1s infinite alternate;
+}
+
+@keyframes shake {
+	from { transform: translate(0, 10px) rotate(0deg); }
+	to { transform: translate(0, 0) rotate(0deg); }
+}
+</style>
 `
 
 
