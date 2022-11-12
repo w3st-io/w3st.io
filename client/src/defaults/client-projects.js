28cc0bb30f45e10fff8e5889200f388af89d5b86
harpoonjs <w3st.io2021@gmail.com>
2022-11-12 15:35:14 -0500

    Improved

--- client/src/defaults/client-projects.js
+++ client/src/defaults/client-projects.js
@@ -10,11 +10,6 @@ module.exports = [
 		link: 'https://aisoni.herokuapp.com' 
 	},
 	{
-		image: 'https://images2.imgbox.com/9d/9f/wCLwHAKF_o.png',
-		title: 'Balance Within',
-		link: 'https://www.balancewithpamela.com' 
-	},
-	{
 		image: 'https://images2.imgbox.com/f8/3f/4VIlG5Um_o.png',
 		title: 'JJ Vinyls',
 		link: 'https://www.jjvinyls.com' 
@@ -25,19 +20,9 @@ module.exports = [
 		link: 'https://dinosbar.herokuapp.com' 
 	},
 	{
-		image: 'https://images2.imgbox.com/96/fc/zsWRlvJu_o.png',
-		title: 'Store JJ Vinyls',
-		link: 'https://shop.jjvinyls.com' 
-	},
-	{
 		image: 'https://images2.imgbox.com/73/09/sZu1UVLO_o.png',
 		title: 'Borghi Fine Art',
 		link: 'https://borghi.herokuapp.com' 
-	},
-	{
-		image: 'https://images2.imgbox.com/f3/8f/iWDuBgBF_o.png',
-		title: 'The Art of Wings',
-		link: 'https://www.theartofwings.com' 
 	},
 	{
 		image: 'https://images2.imgbox.com/d2/1c/LLlHfKcC_o.png',
